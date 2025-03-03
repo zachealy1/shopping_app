616dd4c0f717028add21bbded0b466710dba313f
Zac Healy <zac.healy@hotmail.co.uk>
2025-03-03 10:23:18 +0000

    Added comments to bottom nav bar

--- lib/widgets/bottom_nav_bar.dart
+++ lib/widgets/bottom_nav_bar.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 
+/// A custom BottomNavigationBar widget that displays navigation items for the app.
+/// This widget allows the user to switch between different screens by tapping on the icons.
 class BottomNavBar extends StatelessWidget {
+  /// The index of the currently selected navigation item.
   final int currentIndex;
+
+  /// Callback function that is invoked when a navigation item is tapped.
+  /// The tapped index is passed as a parameter.
   final Function(int) onTap;
 
   const BottomNavBar({super.key, required this.currentIndex, required this.onTap});
@@ -9,11 +15,17 @@ class BottomNavBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
+      // Set the background colour of the bottom navigation bar.
       backgroundColor: const Color(0xFFF1ECF7),
+      // Colour for the selected navigation item.
       selectedItemColor: const Color(0xFF1D2520),
+      // Colour for unselected navigation items.
       unselectedItemColor: Colors.black45,
+      // Indicates which navigation item is currently selected.
       currentIndex: currentIndex,
+      // Callback when a navigation item is tapped.
       onTap: onTap,
+      // Define the navigation items.
       items: const <BottomNavigationBarItem>[
         BottomNavigationBarItem(
           icon: Icon(Icons.place),
